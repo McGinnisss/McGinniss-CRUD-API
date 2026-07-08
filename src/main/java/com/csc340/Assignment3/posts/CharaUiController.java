917b9722442128751af7445490f94e7998ccee4e
[Jacob McGinniss] <J_MCGINNISS@uncg.edu>
2026-07-08 11:30:29 -0400

    Add to ftlh

--- src/main/java/com/csc340/Assignment3/posts/CharaUiController.java
+++ src/main/java/com/csc340/Assignment3/posts/CharaUiController.java
@@ -1,7 +1,9 @@
 package com.csc340.Assignment3.posts;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CharaUiController {
@@ -16,5 +18,13 @@ public class CharaUiController {
     @GetMapping("/about")
     public String about(){
         return "about";
+    }
+
+    @GetMapping("/characters/{id}")
+    public String getCharaById(@PathVariable Long id, Model model) {
+        Character character = charaService.getCharacterById(id);
+        model.addAttribute("character", character);
+
+        return "details";
     }
 }
